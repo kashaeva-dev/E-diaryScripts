74fa70cc03f214eaad17ec532d26219d948533db
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-05-05 17:47:43 +0300

    Add create_commendation() function to create commendation for a given subject and a schoolkid

--- scripts.py
+++ scripts.py
@@ -29,3 +29,54 @@ def remove_chastisements(name):
     chastisements = schoolkid.chastisement_set.all()
     for chastisement in chastisements:
         chastisement.delete()
+
+
+def create_commendation(name, subject):
+    schoolkid = get_schoolkid(name)
+
+    texts = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!',
+    ]
+
+    lesson = Lesson.objects.filter(
+        group_letter=schoolkid.group_letter,
+        year_of_study=schoolkid.year_of_study,
+        subject__title=subject,
+    ).order_by('-date').first()
+
+    Commendation.objects.create(
+        text=random.choice(texts),
+        created=lesson.date,
+        schoolkid=schoolkid,
+        subject=lesson.subject,
+        teacher=lesson.teacher,
+    )
